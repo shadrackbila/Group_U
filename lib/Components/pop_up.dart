a93761291ba0030eab4ab08a2d54097055edcd5f
Shadrack-Bila <shadrackbila2000@gmail.com>
2026-05-09 16:50:51 +0200

    feature: Home page functionality -Create a pop up to show more information about the application -Create buttons to navigate to the edit and reapply screens

---
+++ lib/Components/pop_up.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class PopUp extends StatelessWidget {
+  const PopUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 300,
+      height: 200,
+      padding: EdgeInsets.all(10),
+
+      // Student Card section
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                "Student Assistant",
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(141, 255, 214, 64),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.all(5),
+                child: Text(
+                  "Pending",
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 123, 93, 2),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(141, 64, 150, 255),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.all(5),
+                child: Text(
+                  "2nd Year",
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 2, 91, 123),
+                  ),
+                ),
+              ),
+              SizedBox(width: 20),
+              Container(
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(141, 64, 150, 255),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                padding: EdgeInsets.all(5),
+                child: Text(
+                  "SOD316C",
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 2, 91, 123),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 15),
+          Text("Applied on: 13-05-2026"),
+          SizedBox(height: 15),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Edit",
+                  style: TextStyle(
+                    color: const Color.fromARGB(255, 80, 148, 203),
+                  ),
+                ),
+              ),
+              SizedBox(width: 10),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromARGB(255, 80, 148, 203),
+                ),
+                onPressed: () {},
+                child: Text("Reapply", style: TextStyle(color: Colors.white)),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:group_u/Components/pop_up.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,43 +8,44 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          "Student Assistants",
-          style: TextStyle(color: Colors.white),
-        ),
+        title: Text("My Applications", style: TextStyle(color: Colors.white)),
+
         centerTitle: false,
         backgroundColor: Colors.blue,
       ),
-      body: ListView.builder(
-        itemCount: 7,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text("item #: $index"),
-            subtitle: Text("Status: "),
-            trailing: Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Manage",
-                    style: TextStyle(
-                      color: const Color.fromARGB(255, 80, 148, 203),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 7,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) => Dialog(child: PopUp()),
+                    );
+                  },
+                  title: Text("item #: $index"),
+                  subtitle: Text("Status: "),
+                  trailing: Container(
+                    decoration: BoxDecoration(
+                      color: const Color.fromARGB(141, 255, 214, 64),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: EdgeInsets.all(5),
+                    child: Text(
+                      "Pending",
+                      style: TextStyle(
+                        color: const Color.fromARGB(255, 123, 93, 2),
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(width: 10),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color.fromARGB(255, 80, 148, 203),
-                  ),
-                  onPressed: () {},
-                  child: Text("Submit", style: TextStyle(color: Colors.white)),
-                ),
-              ],
+                );
+              },
             ),
-          );
-        },
+          ),
+        ],
       ),
     );
   }
