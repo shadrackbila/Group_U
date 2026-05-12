0f8ea8dde8b6fcbd03692176cc43f41ed3a1ddf4
Lisakhanya Sandi <144029900+LisaSandi@users.noreply.github.com>
2026-05-12 14:12:36 +0200

    Convert AdminRead to StatefulWidget and implement loading
    Refactor AdminRead widget to use StatefulWidget and load applications on initialization.

--- lib/views/admin_read.dart
+++ lib/views/admin_read.dart
@@ -1,10 +1,74 @@
-import 'package:flutter/material.dart';
 
-class AdminRead extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../viewmodels/admin_viewmodel.dart';
+import '../../models/application_model.dart';
+
+class AdminRead extends StatefulWidget {
   const AdminRead({super.key});
 
   @override
+  State<AdminRead> createState() => _AdminRead();
+}
+
+class _AdminRead  extends State<AdminRead> {
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      context.read<AdminViewModel>().loadAllApplications();
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Admin Read Screen'),
+        backgroundColor: Colors.blue,
+      ),
+      body: Consumer<AdminViewModel>(
+        builder: (context, vm, _) {
+          if (vm.isLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          if (vm.applications.isEmpty) {
+            return const Center(
+              child: Text('No Applications Found'),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: vm.applications.length,
+            itemBuilder: (context, index) {
+              ApplicationModel app = vm.applications[index];
+
+              return Card(
+                margin: const EdgeInsets.all(10),
+                child: ListTile(
+                  leading: CircleAvatar(
+                    child: Text(app.studentName[0]),
+                  ),
+                  title: Text(app.studentName),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(app.studentEmail),
+                      Text('Status: ${app.status}'),
+                      Text('Module: ${app.module1Name}'),
+                    ],
+                  ),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
+
